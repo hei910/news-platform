f98b43b58e13ebc8025b74d67476f2237192106a
DESKTOP-V882DSD\Gordon <gordon.wong@easyview.com.hk>
2021-08-31 17:49:58 +0800

    minior change

--- components/global/Header/index.vue
+++ components/global/Header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <header id="header" class="header">
-    <img id="logo" src="~/assets/img/logo.png" alt="Logo">
+    <!-- <img id="logo" src="~/assets/img/logo.png" alt="Logo"> -->
     <span class="name">資訊發佈平台</span>
   </header>
 </template>
